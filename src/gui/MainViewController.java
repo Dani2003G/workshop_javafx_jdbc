77f063af555c731c63f60372d6f2e0253a1fd14f
Daniel <daniel0202almeida@outlook.com>
2022-03-23 12:34:19 -0300

    DepartmentList viw desing and DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -45,7 +45,7 @@ public class MainViewController implements Initializable {
 	
 	
 	@Override
-	public void initialize(URL uri, ResourceBundle rb) {
+	public void initialize(URL url, ResourceBundle rb) {
 		
 	}	
 	
